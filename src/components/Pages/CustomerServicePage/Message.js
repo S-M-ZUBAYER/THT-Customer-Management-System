4dda4b5d24a2369397f1375cbc76ed0bdbd96135
S-M-ZUBAYER <smzubayer18@gmail.com>
2023-04-17 14:01:54 +0600

    Messaging design part completed

--- src/components/Pages/CustomerServicePage/Message.js
+++ src/components/Pages/CustomerServicePage/Message.js
@@ -4,25 +4,25 @@ const Message = () => {
     return (
         <div         >
 
-            <div className=" my-2 flex gap-0 ">
-                <div className="">
+            <div className=" my-2 flex gap-0 w-full">
+                <div className="w-6">
                     <img className="w-6 h-6 rounded-full" src="https://cdn.pixabay.com/photo/2016/06/03/15/35/customer-service-1433639__340.png"></img>
                
                 </div>
-                <div className="max-w-8/10 ">
-                    <p className="bg-blue-700 px-2 py-1 rounded-tl-none rounded-md text-white">Hello</p>
+                <div className="w-10/12">
+                    <p className=" bg-fuchsia-500 px-2 py-1 rounded-tl-none rounded-md text-white">Hjaskj sdlfa lj jalsjdlfa jsdfl ads;f alj fla lajsfjdlj as fa lfalkj flajf alkjf ajds fjasdjf lkqajdskljalkjlkj qadsj lajs dfaj sjaf lj afd sfajj laj fa a flkja lkfjdlakjflk ds ajsljfs lsakj fdkajsd fadsj flsja lkjfslajlf a jdlfjlfj adfj adfj af jldsfj lafj ajf lkal fajf dj fafdj alfj ldafjs</p>
                     {/* <img className="w-6 h-6 rounded-full" src="https://media.istockphoto.com/id/1244527145/photo/happy-smiling-woman-working-in-call-center.jpg?s=612x612&w=0&k=20&c=3q7ImqhEDOdYptyaZLYmjqtJgMBoY42lGDkzUi0NGBo="></img> */}
 
                 </div>
             </div>
-            <div className=" my-2 flex justify-end gap-0">
-                <div className="max-w-8/10 ">
-                    <p className="bg-blue-700 px-2 py-1 rounded-tr-none rounded-md text-white">Hello</p>
+            <div className=" my-2 flex justify-end  w-full">
+                <div className="w-10/12">
+                    <p className="bg-blue-700 px-2 py-1 rounded-tr-none rounded-md text-white">sjdafk  ajsldkfjla lk ljlakfjd sal lasjdfls al  alsdjf adsalj lj lak fsdjals flka l lkasdjlf lakjd fkla lk jlkjadfslkja fdal jaldsfja fa jlkj afj alj lja f alkajf la lajf lkajl lka jf</p>
                     {/* <img className="w-6 h-6 rounded-full" src="https://media.istockphoto.com/id/1244527145/photo/happy-smiling-woman-working-in-call-center.jpg?s=612x612&w=0&k=20&c=3q7ImqhEDOdYptyaZLYmjqtJgMBoY42lGDkzUi0NGBo="></img> */}
 
                 </div>
-                <div className="">
-                    <img className="w-6 h-6 rounded-full" src="https://cdn.pixabay.com/photo/2016/06/03/15/35/customer-service-1433639__340.png"></img>
+                <div className="w-6">
+                    <img className="w-6 h-6 rounded-full" src="https://media.istockphoto.com/id/1244527145/photo/happy-smiling-woman-working-in-call-center.jpg?s=612x612&w=0&k=20&c=3q7ImqhEDOdYptyaZLYmjqtJgMBoY42lGDkzUi0NGBo="></img>
                    
                 </div>
             </div>
